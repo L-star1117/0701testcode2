75248d2715790ee71b17cdf66fe68b46044977c6
itcast <hello@itcast.cn>
2024-07-01 14:43:15 +0800

    第liu 次修改

--- src/main/java/Test01.java
+++ src/main/java/Test01.java
@@ -5,5 +5,6 @@ public class Test01 {
         System.out.println("test03");
         System.out.println("test04");
         System.out.println("test05");
+        System.out.println("test05");
     }
 }
